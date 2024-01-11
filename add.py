4006f5317d444ecddd7327b88fcc9d1c8ad4bfbc
Shlok011 <151800146+Shlok011@users.noreply.github.com>
2024-01-11 15:25:25 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,9 +1,11 @@
-class parent:
-    def display(self):
-        print("i m a parent")
-class child(parent):
-    def display(self):
-        super().display()
-        print("i m a child")
-obj=child()
-obj.display()
+def max(e,f,g):
+    if(e>f) and (e>g):
+        print("e =",e,"is greater")
+    elif (f>e) and (f>g):
+        print("f =",f,"is greater")
+    else:
+        print("g =",g,"is greater")
+e=int(input("enter e value:"))
+f=int(input("enter f value:"))
+g=int(input("enter g value:"))
+max(e,f,g)
